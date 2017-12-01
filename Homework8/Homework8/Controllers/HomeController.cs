4b65ddc0d398c18e13da2a6f69419d09386694b4
TJ Dennis <tdennis15@mail.wou.edu>
2017-12-01 15:15:11 -0800

    updated the controller and JS file to give us buttons for each genre

--- Homework8/Homework8/Controllers/HomeController.cs
+++ Homework8/Homework8/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Homework8.Models;
 
 namespace Homework8.Controllers
 {
     public class HomeController : Controller
     {
+        private ArtContext db = new ArtContext();
+
         public ActionResult Index()
         {
-            return View();
+
+            var genres = db.Genres;
+            return View(genres);
         }
 
         public ActionResult About()
@@ -25,6 +30,32 @@ namespace Homework8.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        /// <summary>
+        /// Using the following link to understand what the controller and ajax and jquery were needing to talk helped a lot
+        ///     https://stackoverflow.com/questions/25304610/how-to-get-a-list-from-mvc-controller-to-view-using-jquery-ajax
+        /// Since we need to pass a list to the home page instead of a single value means we need to use arrays and loops
+        /// to iterate over the values and convert to a means that the system can handle. 
+        /// This is after the CustomJS.js file has asked for a return. 
+        /// </summary>
+
+        [HttpPost]
+        public JsonResult Genre(string genre)
+        {
+            var artwork = db.Genres.Find(genre).Classifications.ToList().OrderBy(t => t.ArtWork.Title).Select(a => new { aw = a.AWID, awa = a.ArtWork.ArtistID }).ToList();
+            string[] artworkCreator = new string[artwork.Count()];
+            for (int i = 0; i < artworkCreator.Length; ++i)
+            {
+                artworkCreator[i] = $"<ul>{db.ArtWorks.Find(artwork[i].aw).Title} by {db.Artists.Find(artwork[i].awa).ArtistName}</ul>";
+            }
+            var data = new
+            {
+                arr = artworkCreator
+            };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- Homework8/Homework8/Scripts/CustomJS.js
+++ Homework8/Homework8/Scripts/CustomJS.js
@@ -1,1 +1,33 @@
-﻿
+﻿/**
+ * This is the function from the index page to return all the genres found inside the models database
+
+ * @param  genre is passed to help us know when the function is finished and to index our location
+ *thanks to Jake Hatfield for explaining this
+ */
+
+function MakeAJAX_Call(genre) {
+
+    //Resetting the div inside Index to make sure its blanks before we add anything to it (since we arent refreshing the page)
+    document.getElementById('landing_zone').innerHTML = null;
+
+
+    //ajax call
+    $.ajax({
+        //accessing the data inside our Genre view
+        url: "/Home/Genre",
+
+        //finding fields of genre
+        data: { genre: genre },
+        type: "POST",
+
+        //returning the data by iterating over each item found, creating the function and then using the inner function to append it to the 
+        //div found in index
+        success: function (returnData) {
+            returnData.arr.forEach(function (item) {
+                $('#landing_zone').append(item);
+            }
+            )
+        }
+    });
+}
+
